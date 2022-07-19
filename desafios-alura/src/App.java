d6a9eda180eeb415d1ac2c348d2c6cc1eefdfeb1
Hebert Baliza <101848022+Baliza97@users.noreply.github.com>
2022-07-19 10:20:21 -0300

    Update App.java

--- desafios-alura/src/App.java
+++ desafios-alura/src/App.java
@@ -9,55 +9,9 @@ import java.util.Map;
 public class App {
        
         public static void main(String[] args) throws Exception {
-                /*
-                 * // criar cliente
-                 * HttpClient client = HttpClient.newHttpClient();
-                 * HttpRequest requestPopMovies = HttpRequest.newBuilder(
-                 * URI.create("https://imdb-api.com/en/API/MostPopularMovies/" +
-                 * "properties.getKey")).GET()
-                 * .build();
-                 * HttpResponse<String> sResponse = client.send(requestPopMovies,
-                 * BodyHandlers.ofString());
-                 * String jsonBody = sResponse.body();
-                 * // System.out.println(jsonBody);
-                 * 
-                 * JsonParser parser = new JsonParser();
-                 * // List<Map<String, String>> listaFilmesPopulares = parser.parse(jsonBody);
-                 * 
-                 * HttpRequest request250BestTV = HttpRequest.newBuilder(
-                 * URI.create("https://imdb-api.com/en/API/Top250TVs/" +
-                 * "properties.getKey")).GET().build()
-                 * ;
-                 * sResponse = client.send(request250BestTV, BodyHandlers.ofString());
-                 * jsonBody = sResponse.body();
-                 * // System.out.println(jsonBody);
-                 * 
-                 * // List<Map<String, String>> listaMelhoresSeries = parser.parse(jsonBody);
-                 * 
-                 * HttpRequest requestPopSeries = HttpRequest.newBuilder(
-                 * URI.create("https://imdb-api.com/en/API/MostPopularTVs/" +
-                 * "properties.getKey")).GET().
-                 * build();
-                 * sResponse = client.send(requestPopSeries, BodyHandlers.ofString());
-                 * jsonBody = sResponse.body();
-                 * // System.out.println(jsonBody);
-                 * 
-                 * List<Map<String, String>> listaPopSeries = parser.parse(jsonBody);
-                 * 
-                 * // System.out.println(listaFilmesPopulares.size());
-                 * // System.out.println(listaMelhoresSeries.size());
-                 * System.out.println(listaPopSeries.size());
-                 * 
-                 * for (Map<String, String> serie : listaPopSeries) {
-                 * 
-                 * System.out.println(serie.get("title"));
-                 * System.out.println(serie.get("imDbRating"));
-                 * System.out.println("");
-                 * }
-                 */
-
+                
                 // Fazer uma conexão HTTP e buscar top 250 filmes
-                String URL = "https://imdb-api.com/en/API/Top250Movies/" + properties.getKey();
+                String URL = "https://imdb-api.com/en/API/Top250Movies/" + properties.getKey(); //Como a API esta caindo, a API abaixo pode ser utilizada
                 // String URL = "https://alura-filmes.herokuapp.com/conteudos";
                 URI uri = URI.create(URL);
                 HttpClient client = HttpClient.newHttpClient();
@@ -67,8 +21,7 @@ public class App {
                 // System.out.println(body);
 
                 // Transformar os dados
-                // JsonParser parser = new JsonParser();
-                ObjectMapper 
+                JsonParser parser = new JsonParser();
                 List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
                 for (Map<String, String> filme : listaDeFilmes) {
